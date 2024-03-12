a316c3f8b14daaeecd1f8785676950a7f049c71c
Alexandrina <poidaalesya382@gmail.com>
2024-03-12 10:35:20 +0300

    fix: remove profile button from account summary

--- src/components/common/AccountSummary.tsx
+++ src/components/common/AccountSummary.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Box, Card, IconButton, Typography } from '@mui/material';
+import { Box, Card, Typography } from '@mui/material';
 import { GET_ACCOUNT_SUMMARY } from '../../graphql/queries/homeQueries';
 import { useQuery } from '@apollo/client';
-import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 
 const formatCurrency = (amount: number) => {
   return new Intl.NumberFormat('en-US', {
@@ -21,15 +20,9 @@ const AccountSummary = () => {
 
   return (
     <Box sx={{ marginTop: '15px', backgroundColor: '#FFF7F0', width: '100%', padding: '5px', borderRadius: '15px'}}>
-      <Box sx={{ my: 4, display: 'flex', alignItems: 'center', justifyContent: 'space-between', margin: 0 }}>
-        {/* <IconButton edge="start" sx={{ marginRight: 'auto', marginLeft: '0px', padding: '0' }}>
-          <ManageAccountsIcon sx={{ color: '#575757', fontSize: '40px' }}/>
-        </IconButton> */}
-        <Typography variant="h6" component="h1" sx={{ textAlign: 'center', width: '100%' }}>
-          {new Date().toLocaleString('default', { month: 'long' })}
-        </Typography>
-        {/* <Box sx={{ width: 75 }} /> */}
-      </Box>
+      <Typography variant="h6" component="h1" sx={{ textAlign: 'center', width: '100%' }}>
+        {new Date().toLocaleString('default', { month: 'long' })}
+      </Typography>
       <Box display="flex" justifyContent="center" alignItems="center">
         <Card sx={{ display: 'flex', alignItems: 'center', backgroundColor: 'success.main', margin: 1, width: '50%', borderRadius: '23px' }}>
           <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', padding: '10px' }}>
