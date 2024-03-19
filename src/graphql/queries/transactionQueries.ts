32d4883bbce1977a85f31883fec740f2402595c7
Alexandrina <poidaalesya382@gmail.com>
2024-03-19 17:02:45 +0300

    feat: add mutation for getting user transactions

--- src/graphql/queries/transactionQueries.ts
+++ src/graphql/queries/transactionQueries.ts
@@ -10,5 +10,21 @@ export const GET_CATEGORIES = gql`
         id
       }
     }
-}
+  }
 `;
+
+export const GET_TRANSACTIONS = gql`
+  query GetUserTransactions($limit: Float, $startDate: DateTime, $endDate: DateTime, $categoryId: String, $type: TransactionTypeEnum) {
+    getUserTransactions(filters: { limit: $limit, startDate: $startDate, endDate: $endDate, categoryId: $categoryId, type: $type }) {
+      id
+      amount
+      type
+      category {
+        id
+        name
+      }
+      description
+      date
+    }
+  }
+`
